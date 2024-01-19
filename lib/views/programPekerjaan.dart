3cda98bdd44bd2f8bc76a6b2e437a7aa4179586a
gochujjang <41176663+gochujjang@users.noreply.github.com>
2024-01-19 09:21:35 +0700

    fix programpekerjaan

--- lib/views/programPekerjaan.dart
+++ lib/views/programPekerjaan.dart
@@ -83,103 +83,130 @@ class ProgramPekerjaan extends StatelessWidget {
                 thickness: 2,
                 color: Colors.black,
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 100, horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Image(
-                          image: AssetImage(
-                        'lib/assets/pekerjaan-banner.png',
-                      )),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-                          child: Row(
-                            children: [
-                              Text(
-                                "Pekerjaan",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontFamily: 'Poppins',
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 20),
-                                child: Image.asset(
-                                  'lib/assets/emtek.png',
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 17),
-                                child: Text(
-                                  "EMTEK",
-                                  style: TextStyle(
-                                    fontFamily: 'Poppins',
-                                    fontSize: 32,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                          child: Column(
-                            children: [
-                              Text(
-                                "Equipment Technician",
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/detailPekerjaan');
-                                },
-                                child: Text(
-                                  "Daftar",
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 24,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                style: ButtonStyle(
-                                  fixedSize:
-                                      MaterialStateProperty.all(Size(150, 40)),
-                                  backgroundColor: MaterialStateProperty.all(
-                                      AppColors.darkGreen),
-                                  shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
+              listPekerjaan(),
+              listPekerjaan(),
+              listPekerjaan(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class listPekerjaan extends StatelessWidget {
+  const listPekerjaan({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Image(
+                image: AssetImage(
+              'lib/assets/pekerjaan-banner.png',
+            )),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: Row(
+                  children: [
+                    Text(
+                      "Pekerjaan",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Image.asset(
+                        'lib/assets/emtek.png',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 17),
+                      child: Text(
+                        "EMTEK",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 648,
+                margin: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Equipment Technician",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Sebagai Teknisi Peralatan Pertanian, tanggung jawab utama Anda adalah melaksanakan perawatan preventif dan perbaikan peralatan pertanian, termasuk traktor, alat tanam, dan peralatan lainnya. Anda akan memantau kinerja peralatan untuk mendeteksi potensi kerusakan, melakukan diagnosis cepat, dan melaksanakan perbaikan sesuai standar keamanan dan kualitas.",
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/detailPekerjaan');
+                      },
+                      child: Text(
+                        "Daftar",
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w400,
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all(Size(150, 40)),
+                        backgroundColor:
+                            MaterialStateProperty.all(AppColors.darkGreen),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
